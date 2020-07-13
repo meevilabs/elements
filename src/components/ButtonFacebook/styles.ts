a483a885f5decc6ad8923ee71a10354c2e4b935c
Michael Amaral <mike@meevi.dev>
2020-07-13 08:24:08 -0300

    update fb button color

--- src/components/ButtonFacebook/styles.ts
+++ src/components/ButtonFacebook/styles.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components/native';
 import Button from '../Button';
 
 export const Wrapper = styled(Button).attrs({ sound: true })`
-  background-color: #2d4688;
+  background-color: #008be8;
 `;
